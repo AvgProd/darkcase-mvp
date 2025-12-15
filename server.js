584d90a7920056d94a280c9552dcdb9a1dedbc14
Django <ruselle.ltd2@icloud.com>
2025-12-16 00:54:08 +0500

    Add vercel.json configuration

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 const HOST = '0.0.0.0'
 const distPath = path.join(__dirname, 'dist')
+
+app.get('/api/user/library', (req, res) => {
+  console.log('API Hit: User Library for "Моё"')
+  const libraryItems = [
+    { title: 'Черная икра: 1 сезон 1 серия', progress: 'Осталось 50 минут', image: '/images/black_caviar.jpg' },
+    { title: 'Таргет: 1 сезон 1 серия', progress: 'Осталось 23 минуты', image: '/images/target.jpg' },
+    { title: 'Qumalaq', progress: 'Осталось 38 минут', image: '/images/qumalaq.jpg' },
+    { title: 'Слово пацана. Кровь на асфальте: 1 сезон 8 серия', progress: 'Осталось 57 минут', image: '/images/pacan_slovo.jpg' },
+  ]
+  res.json({ success: true, items: libraryItems })
+})
 
 app.use(express.static(distPath, { index: 'index.html', extensions: ['html'] }))
 
