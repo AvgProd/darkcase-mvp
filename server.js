2f0ff2b1326cd949061b55ae2df31e98f00195e9
Django <ruselle.ltd2@icloud.com>
2025-12-16 01:46:10 +0500

    chore: add /api/user/library and set deploy to PORT=3000

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 3000
 const HOST = '0.0.0.0'
 const distPath = path.join(__dirname, 'dist')
 
+app.get('/api/user/library', (req, res) => {
+  const libraryItems = [
+    { title: 'Черная икра: 1 сезон 1 серия', progress: 'Осталось 50 минут', image: '/images/black_caviar.jpg' },
+    { title: 'Таргет: 1 сезон 1 серия', progress: 'Осталось 23 минуты', image: '/images/target.jpg' },
+    { title: 'Qumalaq', progress: 'Осталось 38 минут', image: '/images/qumalaq.jpg' },
+    { title: 'Слово пацана. Кровь на асфальте: 1 сезон 8 серия', progress: 'Осталось 57 минут', image: '/images/pacan_slovo.jpg' },
+  ]
+  res.json({ success: true, items: libraryItems })
+})
+
 app.use(express.static(distPath, { index: 'index.html', extensions: ['html'] }))
 
 app.get('*', (req, res) => {
