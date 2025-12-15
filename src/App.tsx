3a47b05603200c64406c71ab3496348b17f3c113
Django <ruselle.ltd2@icloud.com>
2025-12-15 09:12:21 +0500

    Native App Mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,30 @@ import ShortsPage from './pages/ShortsPage'
 import SearchPage from './pages/SearchPage'
 import ProfilePage from './pages/ProfilePage'
 import CasePage from './pages/CasePage'
-import WebApp from '@twa-dev/sdk'
 
 export default function App() {
   useEffect(() => {
-    WebApp.ready()
-    WebApp.expand()
-    WebApp.setHeaderColor('#000000')
-    WebApp.setBackgroundColor('#000000')
+    if (typeof window !== 'undefined') {
+      type TelegramWebApp = {
+        ready: () => void
+        expand: () => void
+        setHeaderColor: (color: string) => void
+        setBackgroundColor: (color: string) => void
+        isVersionAtLeast?: (v: string) => boolean
+        disableVerticalSwipes?: () => void
+      }
+      const w = window as unknown as { Telegram?: { WebApp?: TelegramWebApp } }
+      const tg = w.Telegram?.WebApp
+      if (tg) {
+        tg.ready()
+        tg.expand()
+        tg.setHeaderColor('#000000')
+        tg.setBackgroundColor('#000000')
+        if (typeof tg.isVersionAtLeast === 'function' && tg.isVersionAtLeast('7.7')) {
+          tg.disableVerticalSwipes()
+        }
+      }
+    }
   }, [])
   return (
     <BrowserRouter>
